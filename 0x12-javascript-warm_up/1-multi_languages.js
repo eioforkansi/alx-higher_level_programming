7ecc258da0406532b4a18603d6ea288292243478
eioforkansi <eioforkansi@gmail.com>
2024-06-19 20:53:19 +0100

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+This script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+*/
+
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
+
+console.log(a);
+console.log(b);
+console.log(c);
