f49b52cbd684024eff2dd4433780307aa3dd2364
eioforkansi <eioforkansi@gmail.com>
2024-06-20 05:39:35 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,9 @@ let message;
 
 if (args.length === 0) {
   message = 'No argument';
-} else {
+} else if (args.length === 1) {
   message = 'Argument found';
+} else {
+  message = 'Arguments found';
 }
 console.log(message);
