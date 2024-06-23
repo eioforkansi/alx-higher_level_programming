f0c2ccebe6de8a059daa1cac3df9330c4136ad25
eioforkansi <eioforkansi@gmail.com>
2024-06-23 17:44:49 +0100

    2. Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+/*
+ * This script has a class Rectangle that defines a rectangle:
+ * The constructor must take 2 arguments w and h
+ * Initialized width with the value of w
+ * Initialized height with the value of h
+ * If w or h is equal to 0 or not a positive integer, create an empty object
+
+*/
+
+class Rectange {
+  constructor (w, h) {
+    if (!(Number.isInteger(w) && w > 0) || !(Number(h) && h > 0)) {
+      //
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectange;
