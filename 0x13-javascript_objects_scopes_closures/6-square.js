f6b4eb169b7f73b5b699839574c6359440e70c04
eioforkansi <eioforkansi@gmail.com>
2024-06-25 13:30:07 +0100

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -16,7 +16,7 @@ module.exports = class Square extends SquareOriginal {
 
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      for (let i = 0; i < this.size; i++) { console.log('X'.repeat(this.size)); }
     } else {
       for (let i = 0; i < this.size; i++) { console.log(c.repeat(this.size)); }
     }
