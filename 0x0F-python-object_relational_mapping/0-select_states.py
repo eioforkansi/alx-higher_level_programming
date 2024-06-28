cdcaea17aef122ccf9fc89e56612d47f111fb851
eioforkansi <eioforkansi@gmail.com>
2024-06-28 14:52:38 +0100

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,17 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database):
     # Connect to MySQL
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
         cursor = db.cursor()
 
         # Execute the query
@@ -27,6 +34,7 @@ def list_states(username, password, database):
         print(f"Error connecting to MySQL: {e}")
         sys.exit(1)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database>")
@@ -37,4 +45,3 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     list_states(username, password, database)
-
