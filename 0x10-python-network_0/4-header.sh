4e2e87dfa9d99aa44cce94b1d011c830faa943f2
eioforkansi <eioforkansi@gmail.com>
2024-07-06 10:38:46 -0700

    9. Catch me if you can!

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # This Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
 curl -sH "X-School-User-Id:98" "${1}"
